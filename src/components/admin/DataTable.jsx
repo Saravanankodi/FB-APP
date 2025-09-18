f5128c8a44fa61d2130b2c9d4023eb566bde8349
desflyer <desflyer.tech@gmail.com>
2025-09-18 14:50:48 +0530

    finished:admin data page

--- src/components/admin/DataTable.jsx
+++ src/components/admin/DataTable.jsx
@@ -6,6 +6,7 @@ import Input from '../base/Input'
 function DataTable() {
   const [users,setUsers] = useState([]);
   const [filters,setFilters] = useState({
+    blood_group:"",
     name:"",
     phone:"",
     country:"",
@@ -29,9 +30,24 @@ function DataTable() {
     return(
       (filters.name === "" || user.name?.toLowerCase().includes(filters.name.toLowerCase())) &&
       (filters.country === "" || user.country?.toLowerCase().includes(filters.country.toLowerCase())) &&
-      (filters.state === "" || user.state?.toLowerCase().includes(filters.state.toLowerCase()))
+      (filters.state === "" || user.state?.toLowerCase().includes(filters.state.toLowerCase())) &&
+      (filters.district === "" || user.district?.toLowerCase().includes(filters.district.toLowerCase())) &&
+      (filters.city === "" || user.city?.toLowerCase().includes(filters.city.toLowerCase())) &&
+      (filters.phone === "" || user.phone?.toLowerCase().includes(filters.phone.toLowerCase())) &&
+      (filters.blood_group === "" || user.blood_group?.toLowerCase().includes(filters.blood_group.toLowerCase()))      
     );
   })
+  const clearFilter = ()=>{
+    setFilters({
+    blood_group:"",
+    name:"",
+    phone:"",
+    country:"",
+    state:"",
+    district:"",
+    city:""
+  })
+  }
   return (
     <section className='p-5'>
       <section className="w-full h-auto grid place-items-center sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5
@@ -40,6 +56,8 @@ function DataTable() {
       name='blood group'
       label="Blood Group"
       type="text"
+      value={filters.blood_group}
+      onchange={(e)=>setFilters({...filters,blood_group:e.target.value})}
       />
       <Input
       name='country'
@@ -52,16 +70,22 @@ function DataTable() {
       name='state'
       label="State"
       type="text"
+      value={filters.state}
+      onchange={(e)=>setFilters({...filters,state:e.target.value})}
       />
       <Input
       name='district'
       label="District"
       type="text"
+      value={filters.district}
+      onchange={(e)=>setFilters({...filters,district:e.target.value})}
       />
       <Input
       name='city'
       label="City"
       type="text"
+      value={filters.city}
+      onchange={(e)=>setFilters({...filters,city:e.target.value})}
       />
       <Input
       name='name'
@@ -74,7 +98,15 @@ function DataTable() {
       name='phone'
       label="Phone"
       type="number"
+      value={filters.phone}
+      onchange={(e)=>setFilters({...filters,phone:e.target.value})}
       />
+      
+      <aside className="col-span-3 justify-self-end">
+        <button onClick={clearFilter} className="btn-primary bg-[#0496ff] text-white rounded-md">
+            Clear Filter
+        </button>
+      </aside>
     </section>
     <table className="w-full h-auto border border-black">
         <thead className="bg-white">
@@ -100,7 +132,7 @@ function DataTable() {
           </tr>
         </thead>
         <tbody>
-          {users.length>0 ? (
+          {filterDate.length>0 ? (
             filterDate.map(user=>(
               <tr key={user.id}>
                 <td className='border border-black text-center'>
@@ -116,7 +148,7 @@ function DataTable() {
                   {user.phone}
                 </td>
                 <td className='border border-black text-center '>
-                  {[user.district, user.city, user.state, user.country].filter(Boolean).join(', ')}
+                  {[user.city,user.district, user.state, user.country].filter(Boolean).join(', ')}
                 </td>
                 <td className='border border-black text-center'>
                   {user.availability}
